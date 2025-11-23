86f9c2beae995a92af84f0fb25b00d179e6caa4a
mohamedEldeeb13 <mooel744@gmail.com>
2025-11-23 13:13:40 +0200

    Initial Commit

---
+++ Social Login DemoTests/Social_Login_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Social_Login_DemoTests.swift
+//  Social Login DemoTests
+//
+//  Created by Mohamed Abd Elhakam on 23/11/2025.
+//
+
+import Testing
+@testable import Social_Login_Demo
+
+struct Social_Login_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
