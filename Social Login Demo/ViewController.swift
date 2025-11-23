f1ad0ff164dfc10f277a81bfb47d1744b5a9d38b
mohamedEldeeb13 <mooel744@gmail.com>
2025-11-24 00:21:37 +0200

    finish apple login integration

--- Social Login Demo/ViewController.swift
+++ Social Login Demo/ViewController.swift
@@ -7,16 +7,18 @@
 
 import UIKit
 import GoogleSignIn
+import AuthenticationServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
     
     //MARK: - IBOutlets -
     @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var appleButton: UIButton!
     
     //MARK: - Life cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-       configuration()
+        configuration()
     }
 }
 
@@ -27,14 +29,17 @@ extension ViewController {
     }
     
     private func setUpInitialUI(){
-        googleButton.layer.cornerRadius = 10
-        googleButton.layer.borderWidth = 1
-        googleButton.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.2).cgColor
-        googleButton.clipsToBounds = true
+        let buttons: [UIButton] = [googleButton, appleButton]
+        buttons.forEach { button in
+            button.layer.cornerRadius = 10
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.2).cgColor
+            button.clipsToBounds = true
+        }
     }
 }
-    
-//MARK: - Actions -
+
+//MARK: - Google Action -
 extension ViewController {
     @IBAction func googleButtonAction(_ sender: Any) {
         
@@ -50,3 +55,37 @@ extension ViewController {
     }
 }
 
+//MARK: - Apple Action -
+extension ViewController {
+    @IBAction func AppleButtonAction(_ sender: Any) {
+        
+        let appleProvider = ASAuthorizationAppleIDProvider()
+        let request = appleProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+}
+
+//MARK: - Apple Delegate functions -
+extension ViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding  {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIdCredintial = authorization.credential as? ASAuthorizationAppleIDCredential {
+            print("User Apple ID: \(appleIdCredintial.user)")
+            print("User Apple Name: \(appleIdCredintial.fullName)")
+            print("User Apple Email: \(appleIdCredintial.email ?? "No Email")")
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Sign in with Apple failed:", error.localizedDescription)
+    }
+    
+}
